bc3f72e8b12610d220f4062c4b5f1b0d85000b7f
a.fedotov <zacyla@gmail.com>
2022-09-30 20:00:17 +0300

    13 hot fix

--- Sem-2/task-13.py
+++ Sem-2/task-13.py
@@ -9,11 +9,15 @@
 str1 = input('Введите строку 1: ')
 str2 = input('Введите строку 2: ')
 count = 0
-k = 1
-for i in range(0, len(str1) - len(str2)+1, k):
-    if str2 in str1[i:i+len(str2)]:
-        count += 1
-        k = len(str2)
-    else:
-        k = 1
+k = 0
+if len(str2) > 1:
+    for i in range(1, len(str1)):
+        if str2 in str1[k:i]:
+            count += 1
+            k = i
+else:
+    for j in range(len(str1)):
+        if str1[j] == str2:
+            count += 1
+
 print(f"Вторая строка входит в первую {count} раз(а).")
