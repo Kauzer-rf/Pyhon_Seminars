b1980bd7aac2d50d6e8d3d7b9fdead4c144194f4
a.fedotov <zacyla@gmail.com>
2022-10-05 19:58:54 +0300

    20 is done

--- Sem-3/task-20.py
+++ Sem-3/task-20.py
@@ -1,1 +1,12 @@
 # 20. Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
+
+n = int(input ('Enter number: '))
+my_list = ['er567', 'iu5', 'tru456t', 'ffg567']
+length = len(my_list)
+n = str(n)
+Flag = False
+for i in range (length):
+    if my_list[i].find(n) !=-1:
+        Flag = True
+        break
+print(Flag)
