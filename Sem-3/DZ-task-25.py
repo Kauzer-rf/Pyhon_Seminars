16e97617818c750323adcbf30ab9c3192439fcb7
a.fedotov <zacyla@gmail.com>
2022-10-07 14:32:27 +0300

    25 2nd varient

--- Sem-3/DZ-task-25.py
+++ Sem-3/DZ-task-25.py
@@ -9,17 +9,21 @@ def cls():
     os.system('cls'if os.name == 'nt' else 'clear')
 cls()
 
-def get_number(a: int):
-    list_ = []
-    while a >= 1:
-        list_.append(a % 2)
-        a = int(a / 2)
-    list_.reverse()
-    bin_number = ''
-    for item in list_:
-        bin_number += str(item)
-    return int(bin_number)
+# def get_number(a: int):
+#     list_ = []
+#     while a >= 1:
+#         list_.append(a % 2)
+#         a = int(a / 2)
+#     list_.reverse()
+#     bin_number = ''
+#     for item in list_:
+#         bin_number += str(item)
+#     return int(bin_number)
 
 
-num = int(input('Введите целое число: '))
-print('Двоичное число =>',get_number(num))
+# num = int(input('Введите целое число: '))
+# print('Двоичное число =>',get_number(num))
+
+# 2nd way
+a = int(input('Enter number: '))
+print(bin(a)[2:])
