470e94bfea0e68cabf46d0c9ccaaf39bbb371108
a.fedotov <zacyla@gmail.com>
2022-10-08 20:20:01 +0300

    30 is done

--- Sem-4/DZ-task-30.py
+++ Sem-4/DZ-task-30.py
@@ -1,3 +1,18 @@
 # Вычислить число π c заданной точностью d
 # Пример:
 # при d = 0.001,π = 3.141             10^(-1)≤d≤10^(-10)
+
+def find_pi(eps=1.0e-5):
+    s=0
+    d=1
+    sgn=1
+    while True:
+        a=4/d
+        s=s+sgn*a
+        if a<eps:
+            return s
+        sgn=-sgn
+        d=d+2
+ 
+ 
+print(find_pi())
