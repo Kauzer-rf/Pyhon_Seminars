4e23e946bf6fdf497eeb97de83fa1c5e18da25e9
a.fedotov <zacyla@gmail.com>
2022-10-08 20:10:33 +0300

    29 is done

--- Sem-4/task-29.py
+++ Sem-4/task-29.py
@@ -1,5 +1,25 @@
 # 29.Задайте два числа. Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел.
 import os
+
+
 def cls():
     os.system('cls'if os.name == 'nt' else 'clear')
+
+
 cls()
+
+a = int(input('a = '))
+b = int(input('b = '))
+
+
+def Efclid(a, b): # Нахождение наибольшего общего делителя (формула Эфклида)
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a+b
+print(Efclid(a, b))
+
+# Находим наименьшее общее кратное
+print(a * b / Efclid(a,b))
