bf841c7fadfc83deab92faa29861d45d4c98a3f3
a.fedotov <zacyla@gmail.com>
2022-10-08 20:47:46 +0300

    32 is done

--- Sem-4/DZ-task-32.py
+++ Sem-4/DZ-task-32.py
@@ -1,5 +1,6 @@
 # 32.	Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 import os
+from random import random
 
 
 def cls():
@@ -7,3 +8,20 @@ def cls():
 
 
 cls()
+
+rnd_list = []
+for i in range(10):
+    rnd_list.append(int(random()*10))
+print(f'Сгенерирована последовательность: {rnd_list}')
+rnd_list.sort()
+print(f'Для удобства отсортивруем ее: {rnd_list}')
+sorted_list = []
+for j in rnd_list:
+    counter = 0
+    for k in rnd_list:
+        if j == k:
+            counter += 1
+    if counter == 1:
+        sorted_list.append(j)
+
+print(f'Список уникальных элементов: {sorted_list}')
