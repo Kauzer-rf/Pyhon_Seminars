5c9652a421475c13e998f9991d2f41ccfbad2892
a.fedotov <zacyla@gmail.com>
2022-10-08 19:41:45 +0300

    28 is done

--- Sem-4/task-28.py
+++ Sem-4/task-28.py
@@ -1,7 +1,29 @@
 # 28.	Найдите корни квадратного уравнения Ax² + Bx + C = 0 двумя способами:
 # 1.	с помощью математических формул нахождения корней квадратного уравнения
-# 2.	с помощью дополнительных библиотек Python
+# 2.	с помощью дополнительных библиотек Python (math)
 import os
+
+
 def cls():
     os.system('cls'if os.name == 'nt' else 'clear')
+
+
 cls()
+
+A = float(input('Enter A: '))
+B = float(input('Enter B: '))
+C = float(input('Enter C: '))
+D = B**2 - 4*A*C
+
+x = 0
+x1 = 0
+
+if D == 0:
+    x = -B/(2*A)
+    print(f'Корень: {x}')
+elif D > 0:
+    x1 = (-B + D ** 0.5) / (2 * A)
+    x = (-B - D ** 0.5) / (2 * A)
+    print(f'Корни: {x} и {x1}')
+else:
+    print('нет корней')
