892d45debe2a45bfb8895a9ba92bc77659490828
a.fedotov <zacyla@gmail.com>
2022-10-10 20:44:14 +0300

    1st init

---
+++ Sem-5/task-1.py
@@ -0,0 +1,17 @@
+"""
+Есть массив, нужно найти элементы, которые не имеют повторений.
+lst = [1, 2, , 3, 5, 1, 5, 3, 10] -> [2, 10]
+"""
+lst = [1, 2, 1, 3, 5, 1, 5, 3, 10]
+
+def sort(lst):
+    uniq_elements = set()
+    for el in lst:
+        if el not in uniq_elements:
+            uniq_elements.add(el)
+        else:
+            uniq_elements.discard(el)
+    s = list(uniq_elements)
+    s.sort()
+    return (s)
+print(sort)
