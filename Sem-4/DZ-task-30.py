0d9be45b5d68eed3af4f5922b064d535e582d09b
a.fedotov <zacyla@gmail.com>
2022-10-10 12:54:58 +0300

    30 и 31 давлено решения

--- Sem-4/DZ-task-30.py
+++ Sem-4/DZ-task-30.py
@@ -10,18 +10,31 @@ def cls():
 
 cls()
 
+# Решение ошибочно
+# def find_pi(eps=1.0e-5):
+#     s = 0
+#     d = 1
+#     sgn = 1
+#     while True:
+#         a = 4/d
+#         s = s+sgn*a
+#         if a < eps:
+#             return s
+#         sgn = -sgn
+#         d = d+2
 
-def find_pi(eps=1.0e-5):
-    s = 0
-    d = 1
-    sgn = 1
-    while True:
-        a = 4/d
-        s = s+sgn*a
-        if a < eps:
-            return s
-        sgn = -sgn
-        d = d+2
 
+# print(f'Ответ: {find_pi()}')
 
-print(f'Ответ: {find_pi()}')
+# Решение верное
+k = 1
+my_pi = 0
+d = input('Введите число d: ')
+for i in range(100000):
+    if i % 2 == 0:
+        my_pi += 4 / k
+    else:
+        my_pi -= 4 / k
+    k += 2
+print(my_pi)
+print(f'\nЧисло П с точностью {d}: {str(my_pi)[:len(d)]}\n')
--- Sem-4/DZ-task-31.py
+++ Sem-4/DZ-task-31.py
@@ -6,6 +6,7 @@
 #  35|5
 #   7|7
 import os
+from re import X
 
 
 def cls():
@@ -13,20 +14,34 @@ def cls():
 
 
 cls()
-N = int(input('Введите натуральное число n: '))
+
+# Решение 1
+# N = int(input('Введите натуральное число n: '))
+# def simple_multiply(n):
+#     i = 2
+#     result_list = []
+#     while i * i <= n:
+#         while n % i == 0:
+#             result_list.append(i)
+#             n = n / i
+#         i = i + 1
+#     if n > 1:
+#         result_list.append(int(n))
+#     return result_list
 
 
-def simple_multiply(n):
-    i = 2
-    result_list = []
-    while i * i <= n:
-        while n % i == 0:
-            result_list.append(i)
-            n = n / i
-        i = i + 1
-    if n > 1:
-        result_list.append(int(n))
-    return result_list
+# print(f'Задано число {N}, список его простых множителей: {simple_multiply(N)}')
 
-
-print(f'Задано число {N}, список его простых множителей: {simple_multiply(N)}')
+# Решение 2
+n = int(input('Введите натуральное число n: '))
+i = 2  # первое простое число
+lst = []
+temp = n
+while i <= temp:
+    if not temp % i:
+        lst.append(i)
+        temp //= i
+        i = 2
+    else:
+        i += 1
+print(f'Задано число {n}, список его простых множителей: {lst}')
