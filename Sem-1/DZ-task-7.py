e26423540bc8ed613108e2e64dd23f25c0a6d5aa
a.fedotov <zacyla@gmail.com>
2022-12-15 21:30:53 +0300

    sem2 init

--- Sem-1/DZ-task-7.py
+++ Sem-1/DZ-task-7.py
@@ -23,11 +23,11 @@
 # result = TrueOrFalse()
 
 # Решение 2
-# for x in range(2):
-#         for y in range(2):
-#             for z in range(2):
-#                 f = not(x or y or z) == (not(x) and not(y) and not(z))
-#                 print(x,y,z,f)
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                f = not(x or y or z) == (not(x) and not(y) and not(z))
+                print(x,y,z,f)
 
 # Решение 3 (от ревьювера)
 # for Z in range(2):
---
+++ Stepik/Beginner/test.py
@@ -0,0 +1,4 @@
+line1 = input()
+line2 = input()
+line3 = input()
+print(f'{line3}\n{line2}\n{line1}')
