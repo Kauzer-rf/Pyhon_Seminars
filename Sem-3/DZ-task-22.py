6f82454724a058d8e7b87f9b84e9ff8d1dc580f0
a.fedotov <zacyla@gmail.com>
2022-12-20 11:15:29 +0300

    sem3 dz 22 pep8 fix

--- Sem-3/DZ-task-22.py
+++ Sem-3/DZ-task-22.py
@@ -29,10 +29,10 @@ cls()
 # print(f' Сумма нечётных элементов: {get_sum(select_index(list_))}')
 
 # _____Решение 2_____
-# spisok = [2, 3, 5, 9, 3]
+# my_list = [2, 3, 5, 9, 3]
 # sum = 0
-# for i in range(1,len(spisok,2)):
-#     sum +=spisok[i]
+# for i in range(1,len(my_list,2)):
+#     sum +=my_list[i]
 # print(sum)
 
 # _____Решение 3_____
