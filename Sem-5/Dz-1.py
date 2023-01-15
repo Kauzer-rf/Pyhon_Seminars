054e46cb50e22cf19381e108e98a8c2253ccf874
a.fedotov <zacyla@gmail.com>
2023-01-15 19:52:36 +0300

    sem5 DZ-1 add 2nd solution

--- Sem-5/Dz-1.py
+++ Sem-5/Dz-1.py
@@ -1,8 +1,13 @@
 """"
-Напишите программу, которая удаляет 'абв' из строки.
+38. Напишите программу, которая удаляет 'абв' из строки.
 """
+# Решение_1
+# my_text = 'Прочитал абв новости абв и абв удивился.'
+# find_text = "абв"
+# lst = [i for i in my_text.split() if find_text not in i]
+# print(f'Результат: {" ".join(lst)}')
 
+# Решение_2
 my_text = 'Прочитал абв новости абв и абв удивился.'
-find_text = "абв"
-lst = [i for i in my_text.split() if find_text not in i]
-print(f'Результат: {" ".join(lst)}')
+new_text = ' '.join(list(filter(lambda elem: 'абв' not in elem.lower(), my_text.split())))
+print(new_text)
