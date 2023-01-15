412635adfb50f96965aa490f9419d284c2f521f4
a.fedotov <zacyla@gmail.com>
2023-01-15 20:49:29 +0300

    sem5 DZ-4 rdy

--- Sem-5/Dz-4.py
+++ Sem-5/Dz-4.py
@@ -1,2 +1,20 @@
 # 42. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+from itertools import groupby
+
+with open('Sem5_Dz-4.txt', 'w') as f:
+     f.write('AAAAAAAABBBBCCCCFF')
+
+with open('Sem5_Dz-4.txt', 'r') as f:
+    my_string = f.read()
+    print(my_string)
+
+def rle(text):
+    return [(key, len(tuple(group))) for key, group in groupby(text)]
+result = rle(my_string)
+print(result)
+
+with open('Sem5_Dz-4(result).txt', 'w') as f:
+    f.write(str(result))
+    
