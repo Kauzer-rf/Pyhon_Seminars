c8b5420885d64f7511624360dcd0c702354dd2e9
a.fedotov <zacyla@gmail.com>
2023-01-27 13:57:54 +0300

    sem7 dz v0.2

--- Sem-7/DZ_Phonebook/view.py
+++ Sem-7/DZ_Phonebook/view.py
@@ -8,3 +8,25 @@ def GetNewPhone():
     return new_phone
 
 def GetUserChoice():
+    return input('Chhose one:\n1. Add new number\n2. Import file\n3. Export file\n4. Exit')
+
+def GetFilePath():
+    return input('Enter path to the file')
+
+def GetExportFileType():
+    return input('Select file type: \n1. csv\n2. txt\n')
+
+def GetDataType(message):
+    print('1. Last_Name_1;First_Name_1;Phone_1;Description_1')
+    print('   Last_Name_2;First_Name_2;Phone_2;Description_2')
+    print()
+    print('2. Last_name_1')
+    print('   First_Name_1')
+    print('   Phone_1')
+    print('   Description_1')
+    print()
+    print('2. Last_name_2')
+    print('   First_Name_2')
+    print('   Phone_2')
+    print('   Description_2')
+    return input(f'Choose the type of data you want to {message}')
