6351fe01ea195ffc45e995efa96c97dd6354c0fb
a.fedotov <zacyla@gmail.com>
2023-01-27 16:23:00 +0300

    sem7 dz v0.7

--- Sem-7/DZ_Phonebook/logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def Log(message):
-    time = datetime.now().strftime('%D %H:%M:%S')
-    with open('log.txt', 'a') as file:
-        file.write('f{time} - {message}\n')
